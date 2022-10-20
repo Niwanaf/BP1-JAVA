eb95763eb97d0d0676cf1308c6c6265920ad3b23
Niwanaf <95495758+Niwanaf@users.noreply.github.com>
2022-10-19 23:29:52 -0700

    bp1

---
+++ BP1_M1_NAFIS/src/BP1_M1_P1_NAFIS.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class BP1_M1_P1_NAFIS {
+    public static void main(String[] args){
+        System.out.println("Bahasa Pemrograman");
+        System.out.println("Sistem Informasi");
+        System.out.println("Fakultas Ilmu Komputer");
+        System.out.println("Universitas Kuningan");
+    }
+}
+/* Hasilnya berjalan dengan sukses*/
---
+++ BP1_M1_NAFIS/src/BP1_M1_P2_NAFIS.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class BP1_M1_P2_NAFIS {
+    public static void main(String[] args){
+        javax.swing.JOptionPane.showMessageDialog(null, "Bahasa Pemrograman 1");
+    }
+    
+}
+
+/* Hasilnya Muncul kotak message dengan isi pesan seperti yang dideklarasikan*/
